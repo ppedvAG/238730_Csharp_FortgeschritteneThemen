2bc436f797dd3e1654df63c9bad4a0b300845cd3
André <AndreR@ppedv.de>
2024-05-29 14:00:26 +0200

    Console mit DI

--- ppedv.CarManager/ppedv.CarManager.UI.DevConsole/Program.cs
+++ ppedv.CarManager/ppedv.CarManager.UI.DevConsole/Program.cs
@@ -1,11 +1,32 @@
-﻿using ppedv.CarManager.Logic;
+﻿using Autofac;
+using ppedv.CarManager.Data.EfCore;
+using ppedv.CarManager.Logic;
 using ppedv.CarManager.Model;
 using ppedv.CarManager.Model.Contracts;
+using System.Reflection;
 
 Console.WriteLine("*** CarManager v0.1 ***");
 
-IRepository repo = new ppedv.CarManager.Data.EfCore.EfRepository();
+//DI per code
+//IRepository repo = new ppedv.CarManager.Data.EfCore.EfRepository();
+//CarServices carServices = new CarServices(repo);
+
+//DI per Reflection, doof wegen abhängigkeiten der abhänigkeiten
+//var path = @"C:\Users\Fred\source\repos\238730_Csharp_FortgeschritteneThemen\ppedv.CarManager\ppedv.CarManager.Data.EfCore\bin\Debug\net8.0\ppedv.CarManager.Data.EfCore.dll";
+//var ass = Assembly.LoadFrom(path);
+//var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+//var repo = (IRepository)Activator.CreateInstance(typeMitRepo);
+//CarServices carServices = new CarServices(repo);
+
+//DI per AutoFac
+var builder = new ContainerBuilder();
+//builder.RegisterType<EfRepository>().AsImplementedInterfaces();
+builder.RegisterType<EfRepository>().As<IRepository>();
+var container = builder.Build();
+
+IRepository repo= container.Resolve<IRepository>();
 CarServices carServices = new CarServices(repo);
+
 
 CreateTestDaten();
 var fastestDriver = carServices.GetDriverWithTheFastestCars();
